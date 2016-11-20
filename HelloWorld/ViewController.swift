2bcac9ef8b3e802c1b4fa96fc888dda24b3803de
Danbala3 <danbala@Js-MacBook-Pro.local>
2016-11-19 19:16:08 -0500

    first commit for Hello-Ayiti app

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    
+    
+    @IBOutlet weak var titleImage: UIImageView!
+    
+    @IBOutlet weak var welcomeBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    @IBAction func welcomePressed(_ sender: AnyObject) {
+        
+        background.isHidden = false
+        titleImage.isHidden = false
+        welcomeBtn.isHidden = true
+        
+    }
+    
+    
 }
 
